66a985452ddad884fffead72739ccae3674a6fb5
KarloMendoza <karlomendoza.@liveramp.com>
2021-08-18 13:56:29 -0500

    first commit

---
+++ src/main/java/TwoSum.java
@@ -0,0 +1,17 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+  public int[] twoSum(int[] nums, int target) {
+    Map<Integer, Integer> diff = new HashMap<>();
+
+    for (int i = 0; i < nums.length ; i++) {
+      if(diff.containsKey(target - nums[i]))
+        return new int[]{i,diff.get(target - nums[i])};
+
+      diff.put(nums[i],i);
+    }
+
+    return null;
+  }
+}
